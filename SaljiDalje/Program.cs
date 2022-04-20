8c6346f85ea305b615a90920c442c27c05aa467c
goransipic <Naranca2019>
2022-04-20 15:35:27 +0200

    prepare setup

--- SaljiDalje/Program.cs
+++ SaljiDalje/Program.cs
@@ -1,12 +1,24 @@
+using Westwind.AspNetCore.LiveReload;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddLiveReload();
+    builder.Services.AddMvc().AddRazorRuntimeCompilation();
+}
 
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{ 
+    app.UseDeveloperExceptionPage();
+    app.UseLiveReload();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
